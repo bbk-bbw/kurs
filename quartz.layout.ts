9115a9c63a46318db370700f1e1fc6a4c6aecfc8
Pietro Rossi <LP4PRossi@gibwintra.ch>
2025-11-02 15:16:27 +0100

    1

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,6 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Explorer(),
     Component.Graph(),
+    //Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
   right: [],
